3eae535b8c2595c22aa0880c720b568cb8d3d0af
Jaehwi-So <sjh9708@gmail.com>
2024-06-24 03:47:26 +0900

    init : 프로젝트 초기설정 및 Elasticsearch 연동

---
+++ src/main/java/com/trusticket/trusticketcontent/TrusticketContentApplication.java
@@ -0,0 +1,32 @@
+package com.trusticket.trusticketcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@ConfigurationPropertiesScan
+@EnableJpaAuditing(dateTimeProviderRef = "dateTimeProvider")
+@SpringBootApplication
+public class TrusticketContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrusticketContentApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+			}
+		};
+	}
+
+}
