aa227a9f209d0401d7f08405f2d8917701517ff9
Jaehwi-So <sjh9708@gmail.com>
2024-06-28 09:14:56 +0900

    feat : MSA 연결

--- src/main/java/com/trusticket/trusticketcontent/TrusticketContentApplication.java
+++ src/main/java/com/trusticket/trusticketcontent/TrusticketContentApplication.java
@@ -3,12 +3,14 @@ package com.trusticket.trusticketcontent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TrusticketContentApplication {
 
 	public static void main(String[] args) {
